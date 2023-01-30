15318ed362f055066ea9b376e98ce2a6f65b72bf
Vinod-Deshmukh <vinod.256.14@gmail.com>
2023-01-30 16:56:36 +0530

    class componenet

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import User from "./User";
+import Users from "./Users";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
       <User />
+      <Users />
     </div>
   );
 }
--- my-app/src/Users.js
+++ my-app/src/Users.js
@@ -1,9 +1,6 @@
-function Users() {
-  return (
-    <div className="App">
-      <h1>Hello Users</h1>
-    </div>
-  );
+import React, { Component } from "react";
+export default class Users extends Component {
+  render() {
+    return <h1>Hello From Class Component</h1>;
+  }
 }
-
-export default Users;
